22ca212e9dde05699960ce6b6044c3fbba13f95b
jonasreyes <jonasrey@gmail.com>
2024-06-20 10:23:02 -0400

    Mejora en configuración de monitor clone o gen erico

--- custom_dp.sh
+++ custom_dp.sh
@@ -17,8 +17,8 @@ monitor_apple_hdmi2=$(xrandr | grep -E "HDMI-2 connected 1680")
 monitor_apple_hdmi3=$(xrandr | grep -E "HDMI-3 connected 1680")
 
 # monitor Mincyt de 22 pulgadas y 1920x1080
-monitor_clone_dp1=$(xrandr | grep -E "DP-1 connected 1920")
-monitor_clone_dp2=$(xrandr | grep -E "DP-2 connected 1920")
+monitor_clone_dp1=$(xrandr | grep -E "DP-1 connected")
+monitor_clone_dp2=$(xrandr | grep -E "DP-2 connected")
 
 pantalla_macbook_rd=$(xrandr | grep -E "eDP-1 connected primary 2560")
 
@@ -63,7 +63,9 @@ if [ -z "$monitor_clone_dp1" ]; then
 else
   echo "Pantalla Clone Conectada"
   echo "Configurando pantalla $monitor_clone_dp1"
-  xrandr --output DP-1 --mode 1920x1080 --scale 2x2 --above eDP-1
+
+  $(xrandr --output eDP-1 --primary --mode 2560x1600 --pos 3840x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off)
+  notify-send "Pantalla Clone configurada en puerto <<$monitor_clone_dp1>>"
 fi
 
 
@@ -71,15 +73,17 @@ if [ -z "$monitor_clone_dp2" ]; then
   echo "Pantalla Clone no conectada"
 else
   echo "Pantalla Clone Conectada"
-  echo "Configurando pantalla $monitor_clone_dp2"
-  xrandr --output DP-2 --mode 1920x1080 --scale 2x2 --above eDP-1
+  notify-send "Configurando pantalla Ultrawide LG en $monitor_clone_dp2"
+  
+  xrandr --output eDP-1 --primary --mode 2560x1600 --pos 1921x0 --rotate normal --output DP-2 --mode 1920x1080 --scale 2x2 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off
+  notify-send "Pantalla Clone configurada en puerto <<$monitor_clone_dp2>>"
 fi
 
 if [ -z "$monitor_ultrawide_hdmi1" ]; then
   echo "Pantalla Ultrawide no conectada"
 else
   echo "Pantalla Ultrawide Conectada"
-  echo "Configurando pantalla $monitor_ultrawide_hdmi1"
+  notify-send "Configurando pantalla Ultrawide LG en $monitor_ultrawide_hdmi1"
 
 # $(cvt 2480 1080 60) # Este comando solo es necesario cuando se desea obtener los valores para el --newmode correcto del comando xrandr.
 $(xrandr --newmode "2480x1080_60.00"  223.50  2480 2640 2904 3328  1080 1083 1093 1120 -hsync +vsync)
@@ -92,8 +96,8 @@ fi
 if [ -z "$monitor_ultrawide_hdmi2" ]; then
   echo "Pantalla Ultrawide no conectada"
 else
-  echo "Pantalla Ultrawide Conectada"
   echo "Configurando pantalla $monitor_ultrawide_hdmi2"
+  notify-send "Configurando pantalla Ultrawide LG en $monitor_ultrawide_hdmi2"
 
 # $(cvt 2480 1080 60) # Este comando solo es necesario cuando se desea obtener los valores para el --newmode correcto del comando xrandr.
 $(xrandr --newmode "2480x1080_60.00"  223.50  2480 2640 2904 3328  1080 1083 1093 1120 -hsync +vsync)
@@ -106,8 +110,7 @@ fi
 if [ -z "$monitor_ultrawide_hdmi3" ]; then
   echo "Pantalla Ultrawide no conectada"
 else
-  echo "Pantalla Ultrawide Conectada"
-  echo "Configurando pantalla $monitor_ultrawide_hdmi3"
+  notify-send "Configurando pantalla Ultrawide LG en $monitor_ultrawide_hdmi3"
 
 # $(cvt 2480 1080 60) # Este comando solo es necesario cuando se desea obtener los valores para el --newmode correcto del comando xrandr.
 $(xrandr --newmode "2480x1080_60.00"  223.50  2480 2640 2904 3328  1080 1083 1093 1120 -hsync +vsync)
@@ -115,3 +118,26 @@ $(xrandr --addmode HDMI-3 2480x1080_60.00)
 $(xrandr --output HDMI-3 --mode 2480x1080_60.00 --scale 2x2 --right-of eDP-1)
 
 fi
+
+# Modos
+# Configuración de monitores por modos
+# por ejemplo: 
+#   Opciones:
+#     -w: significa 'with', indica que se enciende la pantalla nativa del macbook 'con' uno o más monitores externos.
+#     -o: significa 'only', indica que no se enciende la pantalla nativa del macbook y que 'solo' se usará uno u más monitor externo.
+#
+#   Modos: 
+#     -w ultra:   activa macbook con monitor ultrawide, ubicado por defecto right-of (a la derecha)
+#     -w apple:   activa macbook con monitor apple, ubicando por defecto el monitor externo above (arriba)
+#     -w clone:   activa macbook con monitor clone, ubicando por defecto el monitor externo left-of (a la izquierda)
+#     -w full:    activa macbook abajo, clone arriba a la derecha, apple arriba al centro y ultrawide arriba a la derecha
+#
+#     -o ultra: activa solo el monitor ultrawide
+#     -o apple: activa solo el monitor apple
+#     -o clone: activa solo el monitor clone
+#     -o full:  activa todos los monitores detectados, uno al lado de otro y dejando apagada solo la pantalla del macbook
+#
+#     -home:    modo combo, monitores por defecto desde casa.
+#     -work:    modo combo, monitores por defecto desde el trabajo.
+#     -devhome: modo combo para desarrollar desde casa.
+#     -devwork: modo combo para desarrollar desde el trabajo.
